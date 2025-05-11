65ddfc62af59039f271d36651c5ac621e4d9d345
Antanas Jakubėnas <antanas.jakubenas@vmvt.lt>
2025-05-11 12:10:07 +0300

    UPD - med_idf is not null

--- Modules/UPD/Upd/Methods.cs
+++ Modules/UPD/Upd/Methods.cs
@@ -37,7 +37,7 @@ public static partial class UpdMedicines {
 			JsonField = en ? "list_en" : "list_lt",
 			//TODO: Filtrai
 			//Where = (!ctx.ParamNull("org") || !ctx.ParamNull("country")) ? new() { OrgID = ctx.ParamString("org"), CountryCode = ctx.ParamString("country")?.ToUpper() } : null,
-			WhereAdd = ctx.ParamString("order") is null ? null : "\"med_date\" is not null",
+			WhereAdd = "med_idf is not null",
 			Fields = ListFld,
 			Select = en ? ListEn : ListLt
 		}.Execute();
